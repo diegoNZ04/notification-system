d8b47cf40a4b45cc50347c1c503a2bf4b430f5fa
diegoNZ04 <diegoamorim03152004@gmail.com>
2025-01-15 15:05:43 -0300

    initial setup

---
+++ NotificationSystem.API/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+
+app.Run();
